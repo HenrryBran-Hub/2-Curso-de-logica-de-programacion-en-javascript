b338b142702364ee866380d289e58715a0a8f6eb
HENRRY DAVID BRAN VELASQUEZ <2168036320108@ingenieria.usac.edu.gt>
2024-01-26 22:49:36 -0600

    Desafio
    Desafio No. 3

--- app.js
+++ app.js
@@ -1,257 +1,72 @@
 /*
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = `¡Adivina el número! Elige un número entre 1 y 10`;
-
-function asignarTextoElemento(elemento, texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-}
-
-function intentoDeUsuario() {
-    alert("Click desde el boton");
-}
-
-
-*/
-/*
-function asignarTextoElemento(elemento, texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-}
-
-function intentoDeUsuario() {
-    alert("Click desde el boton");
-}
-
-asignarTextoElemento('h1','Juego del número secreto!');
-asignarTextoElemento('p','Indica un número del 1 al 100');
-
-*/
-
-/*
-let numeroSecreto = generarNumeroSecreto();
-
-console.log(numeroSecreto);
-
-function asignarTextoElemento(elemento, texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-function intentoDeUsuario() {
-    alert("Click desde el boton");
-    return;
-}
-
-function generarNumeroSecreto() {
-    return Math.floor(Math.random()*10)+1;
-}
-
-asignarTextoElemento('h1','Juego del número secreto!');
-asignarTextoElemento('p','Indica un número del 1 al 10');
-*/
-/*
-let numeroSecreto = generarNumeroSecreto();
-
-function asignarTextoElemento(elemento, texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    console.log(typeof(numeroDeUsuario));
-    console.log(numeroSecreto);
-    console.log(typeof(numeroSecreto));
-    console.log(numeroDeUsuario);
-    console.log(numeroSecreto === numeroDeUsuario);
-    return;
-}
-
-function generarNumeroSecreto() {
-    return Math.floor(Math.random()*10)+1;
-}
-
-asignarTextoElemento('h1','Juego del número secreto!');
-asignarTextoElemento('p','Indica un número del 1 al 10');
-*/
-/*
 Desafíos:
-Crear una función que muestre "¡Hola, mundo!" en la consola.
-Crear una función que reciba un nombre como parámetro y muestre "¡Hola, [nombre]!" en la consola.
-Crear una función que reciba un número como parámetro y devuelva el doble de ese número.
-Crear una función que reciba tres números como parámetros y devuelva su promedio.
-Crear una función que reciba dos números como parámetros y devuelva el mayor de ellos.
-Crear una función que reciba un número como parámetro y devuelva el resultado de multiplicar ese número por sí mismo.
-
+1.Crea una función que calcule el índice de masa corporal (IMC) de una persona a partir de 
+su altura en metros y peso en kilogramos, que se recibirán como parámetros.
+2.Crea una función que calcule el valor del factorial de un número pasado como parámetro.
+3.Crea una función que convierta un valor en dólares, pasado como parámetro, 
+y devuelva el valor equivalente en reales(moneda brasileña,si deseas puedes hacerlo con el valor del dólar en tu país). 
+Para esto, considera la cotización del dólar igual a R$4,80.
+4.Crea una función que muestre en pantalla el área y el perímetro de una sala rectangular, 
+utilizando la altura y la anchura que se proporcionarán como parámetros.
+5.Crea una función que muestre en pantalla el área y el perímetro de una sala circular, 
+utilizando su radio que se proporcionará como parámetro. Considera Pi = 3,14.
+6.Crea una función que muestre en pantalla la tabla de multiplicar de un número dado como parámetro.
 */
-/*
-function hola() {
-    console.log('¡Hola, mundo!');
+
+//1.
+function imc(altura, peso) {
+    return peso / Math.pow(altura, 2);
 }
 
-function holaParametro(elemento) {
-    console.log(`¡Hola, ${elemento}!`);
-}
+let indice = imc(1.78, 286);
+console.log("El IMC es " + indice);
 
-function numeroDoble(numero) {
-    return numero * 2;
-}
-
-function promedio(numero1, numero2, numero3) {
-    return (numero1 + numero2 + numero3) / 3;
-}
-
-function maximo(numero1, numero2) {
-    if (numero1 > numero2) {
-        return numero1;
+//2.
+function factorial(numero) {
+    if (numero === 0 || numero === 1) {
+        return 1;
     } else {
-        return numero2;
+        return numero * factorial(numero - 1);
     }
 }
 
-function maximoVdos(numero1, numero2){
-    return numero1 > numero2 ? numero1 : numero2;
+let numeroFactorial = factorial(5);
+console.log("El factorial de 5 es " + numeroFactorial);
+
+//3.
+function dolares(moneda) {
+    return moneda * 4.80;
 }
 
-function multiplicar(numero){
-    return numero * numero;
+let dinero = dolares(100);
+console.log("Los $100 son " + dinero);
+
+//4.
+function areaRectangulo(ancho, largo) {
+    let base = ancho * largo;
+    let perimetro = largo * 2 + ancho * 2;
+    console.log(`El area del rectangulo es:  ${base}`);
+    console.log(`El perimetro del rectangulo es:  ${perimetro}`);
 }
 
-hola();
-holaParametro('Henrry');
-console.log(numeroDoble(2));
-console.log(promedio(5, 10, 15));
-console.log(maximo(25,30));
-console.log(multiplicar(10));
-console.log(maximoVdos(30,5));
+areaRectangulo(5, 5);
 
-*/
-/*
-let numeroSecreto = generarNumeroSecreto();
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
+//5.
+function areaCirculo(radio) {
+    let pi = 3.14;
+    let area = pi * radio * radio;
+    let perimetro = 2 * pi * radio;
+    console.log('area  del circulo '+ area);
+    console.log('Perimetro del Circulo ' + perimetro);
 }
 
-function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+areaCirculo(3);
 
-    console.log(numeroSecreto);
-
-    if (numeroSecreto === numeroDeUsuario) {
-        asignarTextoElemento('p', 'Acertaste el número');
-    } else {
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p', 'El número secreto es menor');
-        } else {
-            asignarTextoElemento('p', 'El número secreto es mayor');
-        }
+//6.
+function tabladeMutiplicar(numero){
+    for (var i=1;i<=10;i++){
+        console.log(i +" x "+numero+" = "+(i*numero));
     }
-    return;
 }
 
-function generarNumeroSecreto() {
-    return Math.floor(Math.random() * 10) + 1;
-}
-
-asignarTextoElemento('h1', 'Juego del número secreto!');
-asignarTextoElemento('p', 'Indica un número del 1 al 10');
-*/
-
-/*
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
-console.log(numeroSecreto);
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-
-    if (numeroSecreto === numeroDeUsuario) {
-        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'ves' : 'veces' }`);
-    } else {
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p', 'El número secreto es menor');
-        } else {
-            asignarTextoElemento('p', 'El número secreto es mayor');
-        }
-        intentos++;
-    }
-    return;
-}
-
-function generarNumeroSecreto() {
-    return Math.floor(Math.random() * 10) + 1;
-}
-
-asignarTextoElemento('h1', 'Juego del número secreto!');
-asignarTextoElemento('p', 'Indica un número del 1 al 10');
-*/
-
-
-let numeroSecreto = 0;
-let intentos = 0;
-console.log(numeroSecreto);
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-
-    if (numeroSecreto === numeroDeUsuario) {
-        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'ves' : 'veces' }`);
-        document.getElementById('reiniciar').removeAttribute('disabled');
-    } else {
-        //El usuario no acertó.
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p', 'El número secreto es menor');
-        } else {
-            asignarTextoElemento('p', 'El número secreto es mayor');
-        }
-        intentos++;
-        limpiarCaja();
-    }
-    return;
-}
-
-function limpiarCaja() {
-    document.querySelector('#valorUsuario').value = '';
-}
-
-function generarNumeroSecreto() {
-    return Math.floor(Math.random() * 10) + 1;
-}
-
-function condicionesIniciales() {
-    asignarTextoElemento('h1', 'Juego del número secreto!');
-    asignarTextoElemento('p', 'Indica un número del 1 al 10');
-    numeroSecreto = generarNumeroSecreto();
-    intentos = 1;
-}
-
-function reiniciarJuego() {
-    //Limpiar caja
-    limpiarCaja();
-    //Indicar mensaje de intervalo de numeros
-    //Generar el número aleatorio    
-    //Inicializar el número de intentos
-    condicionesIniciales();    
-    //Deshabilitar el botón de nuevo juego
-    document.querySelector('#reiniciar').setAttribute('disabled','true');
-}
-
-condicionesIniciales();
+tabladeMutiplicar(5)
