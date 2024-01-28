408aa1798e3f7851f7c2581a764f7c4f947fcec9
Henrry Bran <80603803+HenrryBran-Hub@users.noreply.github.com>
2024-01-27 23:24:39 -0600

    4 parte

--- app.js
+++ app.js
@@ -1,72 +1,56 @@
-/*
-Desafíos:
-1.Crea una función que calcule el índice de masa corporal (IMC) de una persona a partir de 
-su altura en metros y peso en kilogramos, que se recibirán como parámetros.
-2.Crea una función que calcule el valor del factorial de un número pasado como parámetro.
-3.Crea una función que convierta un valor en dólares, pasado como parámetro, 
-y devuelva el valor equivalente en reales(moneda brasileña,si deseas puedes hacerlo con el valor del dólar en tu país). 
-Para esto, considera la cotización del dólar igual a R$4,80.
-4.Crea una función que muestre en pantalla el área y el perímetro de una sala rectangular, 
-utilizando la altura y la anchura que se proporcionarán como parámetros.
-5.Crea una función que muestre en pantalla el área y el perímetro de una sala circular, 
-utilizando su radio que se proporcionará como parámetro. Considera Pi = 3,14.
-6.Crea una función que muestre en pantalla la tabla de multiplicar de un número dado como parámetro.
-*/
+let numeroSecreto = 0;
+let intentos = 0;
+console.log(numeroSecreto);
 
-//1.
-function imc(altura, peso) {
-    return peso / Math.pow(altura, 2);
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
 
-let indice = imc(1.78, 286);
-console.log("El IMC es " + indice);
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
 
-//2.
-function factorial(numero) {
-    if (numero === 0 || numero === 1) {
-        return 1;
+    if (numeroSecreto === numeroDeUsuario) {
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'ves' : 'veces' }`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
-        return numero * factorial(numero - 1);
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El número secreto es menor');
+        } else {
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
     }
+    return;
 }
 
-let numeroFactorial = factorial(5);
-console.log("El factorial de 5 es " + numeroFactorial);
-
-//3.
-function dolares(moneda) {
-    return moneda * 4.80;
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
 }
 
-let dinero = dolares(100);
-console.log("Los $100 son " + dinero);
-
-//4.
-function areaRectangulo(ancho, largo) {
-    let base = ancho * largo;
-    let perimetro = largo * 2 + ancho * 2;
-    console.log(`El area del rectangulo es:  ${base}`);
-    console.log(`El perimetro del rectangulo es:  ${perimetro}`);
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
 }
 
-areaRectangulo(5, 5);
-
-//5.
-function areaCirculo(radio) {
-    let pi = 3.14;
-    let area = pi * radio * radio;
-    let perimetro = 2 * pi * radio;
-    console.log('area  del circulo '+ area);
-    console.log('Perimetro del Circulo ' + perimetro);
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del número secreto!');
+    asignarTextoElemento('p', 'Indica un número del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
 }
 
-areaCirculo(3);
-
-//6.
-function tabladeMutiplicar(numero){
-    for (var i=1;i<=10;i++){
-        console.log(i +" x "+numero+" = "+(i*numero));
-    }
+function reiniciarJuego() {
+    //Limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de numeros
+    //Generar el número aleatorio    
+    //Inicializar el número de intentos
+    condicionesIniciales();    
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
 }
 
-tabladeMutiplicar(5)
+condicionesIniciales();
